3f36eda36e45efd02943083a8c9a663d42ab132e
Marc Ferret <marc.ferret@mango.com>
2019-09-30 15:44:27 +0200

    Updated mngForm component

--- src/mngForm/components/mngForm.js
+++ src/mngForm/components/mngForm.js
@@ -2,8 +2,13 @@
 export default class MngForm extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
       <h1>Form!!!!</h1>
-    `
+      <form>
+        <input id="update-header" placeholder="Introduce el nuevo titulo:" value="" />
+        <button type="submit">Change Title</button>
+      </form>
+    `;
   }
 }
