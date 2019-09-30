b6395e0a8936318270c9bea4ea196a3f6359ca5b
Marc Ferret <marc.ferret@mango.com>
2019-09-30 16:27:09 +0200

    Added custom event to send input value

--- src/mngForm/components/mngForm.js
+++ src/mngForm/components/mngForm.js
@@ -1,14 +1,21 @@
-
 export default class MngForm extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
       <h1>Form!!!!</h1>
-      <form>
-        <input id="update-header" placeholder="Introduce el nuevo titulo:" value="" />
-        <button type="submit">Change Title</button>
-      </form>
+      <input id="updateHeader" placeholder="Introduce el nuevo titulo:" value="" />
+      <button id="updateHeaderButton">Change Title</button>
     `;
+    this.changeTitle();
+  }
+
+  changeTitle() {
+    const { shadowRoot } = this;
+    shadowRoot.querySelector('#updateHeaderButton').addEventListener('click', () => {
+      const inputValue = shadowRoot.querySelector('#updateHeader').value;
+      const changeTitleEvent = new CustomEvent('mngForm.changeTitle', { detail: { title: inputValue } });
+      window.dispatchEvent(changeTitleEvent);
+    });
   }
 }
