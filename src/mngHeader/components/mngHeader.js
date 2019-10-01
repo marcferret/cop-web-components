0ef7f97f6b56454f825e86d292457c891d5e24e2
Xavi Molina <xavier.molina@mango.com>
2019-10-01 16:50:45 +0200

    styles added

--- src/mngHeader/components/mngHeader.js
+++ src/mngHeader/components/mngHeader.js
@@ -4,7 +4,23 @@ export default class MngHeader extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
-      <h1 id="title">Hello!!!</h1>
+    <style>      
+      .header {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        width: 100%;
+      }
+      
+      h1 {
+        color: #13293D;
+      }
+    </style>
+      <div class="header">
+        <h1>Welcome to WebComponents CoP!</h1>
+        <h2 id="title">Change me! I'm the title.</h2>
+      </div>
     `;
   }
 }
